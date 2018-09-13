754e0713e259a5a095562336580feee7d08e653b
Gagan Chohan <gaganchohan@icloud.com>
2018-09-13 15:46:55 +1000

    Changed ProductRepo

--- src/main/java/com/productmicro/micro/Repositories/ProductRepository.java
+++ src/main/java/com/productmicro/micro/Repositories/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.productmicro.micro.Repositories;
 
 import com.productmicro.micro.Entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -21,6 +22,7 @@ public interface ProductRepository extends CrudRepository<Product, Integer> {
     // Sort by name
     @Query("SELECT p from Product p order by p.name asc") //figure out how to replace ?1 with rating
     List<Product> sortByName();
+
     //multiple entries
     List <Product> findProductByCategoryName(String category);
     //only one will be returned as Products are unique
